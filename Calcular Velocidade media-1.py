9de80e8d9471ea29bf8b697fb979ff9911359c83
ArthurMenezes09 <arthurmenezess153@gmail.com>
2025-05-28 18:28:09 -0300

    Calcular velocidade media
    Este projeto é uma calculadora de velocidade média desenvolvida com o objetivo de praticar e aprimorar meus conhecimentos em programação e conceitos básicos de Física. A aplicação permite calcular a velocidade média de um objeto com base na distância percorrida e no tempo de horas e minutos gasto.

---
+++ Calcular Velocidade media-1.py
@@ -0,0 +1,11 @@
+
+print("Simulador de computador de bordo")
+distancia = float(input("Digite a distancia percorrida (em km): "))
+horas = float(input("Digite o tempo gasto (horas): "))
+minutos = float(input("Digite o tempo gasto (minutos): "))
+tempo = horas + minutos / 60
+velocidade_media = distancia / tempo
+print(f"A velocidade media foi de {velocidade_media:.2f} km/h")
+print("Obrigado por usar o simulador!")
+print("Volte sempre!")
+print("Arthur Menezes. vou conseguir! 2024")

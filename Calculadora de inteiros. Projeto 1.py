5caaa2d23528c758efc8f88b9c918d358e545a11
ArthurMenezes09 <arthurmenezess153@gmail.com>
2025-05-28 17:22:24 -0300

    Calculadora de inteiros
    Este projeto é uma calculadora de inteiros desenvolvida com o objetivo de praticar e aprimorar meus conhecimentos em programação. A aplicação permite realizar operações básicas como adição, subtração, multiplicação e divisão entre números inteiros

---
+++ Calculadora de inteiros. Projeto 1.py
@@ -0,0 +1,12 @@
+
+print("Boas vindas ao calculador de inteiros")
+primeiro_valor = int(input("Por favor, informe o primeiro valor: "))
+segundo_valor = int(input("Por favor, informe o segundo valor: "))
+soma = primeiro_valor + segundo_valor
+print("A soma dos valores é " + str(soma))
+subtracao = primeiro_valor - segundo_valor
+print(" A subtração dos valores é " + str(subtracao))
+divisao = primeiro_valor / segundo_valor
+print(" A divisão dos valores eé " + str(divisao))
+multiplicacao = primeiro_valor * segundo_valor
+print(" A multiplicação dos valores é " + str(multiplicacao))
